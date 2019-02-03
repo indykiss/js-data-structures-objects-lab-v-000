e7faf00a8075b31e09fd40da97ff1f2f674ba5d6
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-03 18:06:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,6 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 
 function deleteFromDriverByKey(driver, key) {
   newDriver = Object.assign ({}, driver);
-  
+  delete newDriver[key];
+  return newDriver;
 }
